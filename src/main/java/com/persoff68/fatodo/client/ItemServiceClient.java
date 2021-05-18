09fab7c2da8566477c1981e61f9ba0eb2db5ed58
Vladimir Perfilev <persoff68@gmail.com>
2021-05-18 12:53:22 +0200

    TDM-88 fix item service client

--- src/main/java/com/persoff68/fatodo/client/ItemServiceClient.java
+++ src/main/java/com/persoff68/fatodo/client/ItemServiceClient.java
@@ -3,7 +3,6 @@ package com.persoff68.fatodo.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,10 +10,10 @@ import java.util.UUID;
 @FeignClient(name = "item-service", primary = false)
 public interface ItemServiceClient {
 
-    @PostMapping(value = "/api/permissions/group/{groupId}")
+    @GetMapping(value = "/api/permissions/group/{groupId}")
     boolean canReadGroup(@PathVariable UUID groupId);
 
-    @PostMapping(value = "/api/permissions/item/{itemId}")
+    @GetMapping(value = "/api/permissions/item/{itemId}")
     boolean canReadItem(@PathVariable UUID itemId);
 
     @GetMapping(value = "/api/check/is-group/{groupId}")
