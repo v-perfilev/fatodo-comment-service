78d2abb479b18bc6f5c2bcc707a9eaafaacf0261
Vladimir Perfilev <persoff68@gmail.com>
2022-08-22 17:01:45 +0200

    TDM-153 fix ws contract test

--- src/test/java/com/persoff68/fatodo/contract/WsServiceCT.java
+++ src/test/java/com/persoff68/fatodo/contract/WsServiceCT.java
@@ -1,19 +1,13 @@
 package com.persoff68.fatodo.contract;
 
-import com.persoff68.fatodo.builder.TestCommentDTO;
-import com.persoff68.fatodo.builder.TestReactionsDTO;
-import com.persoff68.fatodo.builder.TestWsEventDTO;
+import com.persoff68.fatodo.builder.TestWsEventWithUsersDTO;
 import com.persoff68.fatodo.client.WsServiceClient;
-import com.persoff68.fatodo.model.dto.CommentDTO;
-import com.persoff68.fatodo.model.dto.ReactionsDTO;
-import com.persoff68.fatodo.model.dto.WsEventDTO;
+import com.persoff68.fatodo.model.dto.WsEventWithUsersDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -26,25 +20,9 @@ class WsServiceCT {
     WsServiceClient wsServiceClient;
 
     @Test
-    void testSendCommentNewEvent() {
-        CommentDTO commentDTO = TestCommentDTO.defaultBuilder().id(UUID.randomUUID()).build().toParent();
-        WsEventDTO<CommentDTO> dto = TestWsEventDTO.<CommentDTO>defaultBuilder().content(commentDTO).build().toParent();
-        assertDoesNotThrow(() -> wsServiceClient.sendCommentNewEvent(dto));
-    }
-
-    @Test
-    void testSendCommentUpdateEvent() {
-        CommentDTO commentDTO = TestCommentDTO.defaultBuilder().id(UUID.randomUUID()).build().toParent();
-        WsEventDTO<CommentDTO> dto = TestWsEventDTO.<CommentDTO>defaultBuilder().content(commentDTO).build().toParent();
-        assertDoesNotThrow(() -> wsServiceClient.sendCommentUpdateEvent(dto));
-    }
-
-    @Test
-    void testSendReactionsEvent() {
-        ReactionsDTO reactionsDTO = TestReactionsDTO.defaultBuilder().build().toParent();
-        WsEventDTO<ReactionsDTO> dto = TestWsEventDTO.<ReactionsDTO>defaultBuilder()
-                .content(reactionsDTO).build().toParent();
-        assertDoesNotThrow(() -> wsServiceClient.sendReactionsEvent(dto));
+    void testSendEvent() {
+        WsEventWithUsersDTO dto = TestWsEventWithUsersDTO.defaultBuilder().build();
+        assertDoesNotThrow(() -> wsServiceClient.sendEvent(dto));
     }
 
 }
